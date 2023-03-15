2aa4bf6c64f41fdbdc9ee81af31f0d5fe4aff6ff
DendyPol <Polovinko_91@icloud.com>
2023-03-15 15:48:38 +0300

    five task re4

--- src/lesson_five/ThreadHomeWork.java
+++ src/lesson_five/ThreadHomeWork.java
@@ -14,39 +14,39 @@ public class ThreadHomeWork {
     secondMethod();
   }
 
-  private static void formula(float i) {
-    i = (float) (i * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+  private static Double formula(Float i) {
+    return i * (i * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
   }
 
   public static void firstMethod() {
     long startTime = System.currentTimeMillis();
-    var arr = new float[size];
-    Arrays.fill(arr, 1);
+    var arr = new Float[size];
+    Arrays.fill(arr, 1.0f);
     for (int i = 0; i < arr.length; i++) {
-      formula(i);
+      formula(Float.valueOf(i));
     }
     System.out.printf("Время выполнения однопоточности: %d\n", System.currentTimeMillis() - startTime);
   }
 
   public static void secondMethod() {
     long startTime = System.currentTimeMillis();
-    var initialArray = new float[size];
-    Arrays.fill(initialArray, 1);
-    var leftHalf = new float[h];
-    var rightHalf = new float[h];
+    var initialArray = new Float[size];
+    Arrays.fill(initialArray, 1.0f);
+    var leftHalf = new Float[h];
+    var rightHalf = new Float[h];
     System.arraycopy(initialArray, 0, leftHalf, 0, h);
     System.arraycopy(initialArray, h, rightHalf, 0, h);
     var firstThread = new Thread(() -> {
       for (int i = 0; i < leftHalf.length; i++) {
-        formula(i);
+        formula(Float.valueOf(i));
       }
     });
     var secondThread = new Thread(() -> {
       for (int i = 0; i < rightHalf.length; i++) {
-        formula(i);
+        formula(Float.valueOf(i));
       }
     });
-    var mergedArray = new float[size];
+    var mergedArray = new Float[size];
     System.arraycopy(leftHalf, 0, mergedArray, 0, h);
     System.arraycopy(rightHalf, 0, mergedArray, h, h);
     firstThread.start();
