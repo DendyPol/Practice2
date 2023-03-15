33f2eea57c7923ada9bb3208ac8fa4a20b0bc663
DendyPol <Polovinko_91@icloud.com>
2023-03-15 15:10:07 +0300

    five task re3

--- src/lesson_five/ThreadHomeWork.java
+++ src/lesson_five/ThreadHomeWork.java
@@ -14,8 +14,8 @@ public class ThreadHomeWork {
     secondMethod();
   }
 
-  private static void formula(float[] arr) {
-    arr[(int) (float) 0] = (float) (arr[(int) (float) 0] * Math.sin(0.2f + (float) 0 / 5) * Math.cos(0.2f + (float) 0 / 5) * Math.cos(0.4f + (float) 0 / 2));
+  private static void formula(float i) {
+    i = (float) (i * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
   }
 
   public static void firstMethod() {
@@ -23,7 +23,7 @@ public class ThreadHomeWork {
     var arr = new float[size];
     Arrays.fill(arr, 1);
     for (int i = 0; i < arr.length; i++) {
-      formula(arr);
+      formula(i);
     }
     System.out.printf("Время выполнения однопоточности: %d\n", System.currentTimeMillis() - startTime);
   }
@@ -38,12 +38,12 @@ public class ThreadHomeWork {
     System.arraycopy(initialArray, h, rightHalf, 0, h);
     var firstThread = new Thread(() -> {
       for (int i = 0; i < leftHalf.length; i++) {
-        formula(leftHalf);
+        formula(i);
       }
     });
     var secondThread = new Thread(() -> {
       for (int i = 0; i < rightHalf.length; i++) {
-        formula(rightHalf);
+        formula(i);
       }
     });
     var mergedArray = new float[size];
@@ -55,8 +55,7 @@ public class ThreadHomeWork {
       firstThread.join();
       secondThread.join();
     } catch (InterruptedException e) {
-      logger.log(Level.WARNING, "Возникла ошибка чтения потока:" + e.getMessage());
-      e.printStackTrace();
+      logger.log(Level.WARNING, "Возникла ошибка чтения потока:" + e.getMessage() + Arrays.toString(e.getStackTrace()));
     }
     System.out.printf("Время выполения многопоточности: %d\n", System.currentTimeMillis() - startTime);
   }
